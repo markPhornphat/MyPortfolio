12649fd34e561cfea8b76bcdf91c1d70ea983768
marklileo <phornphatmak1112@gmail.com>
2023-12-16 18:53:55 +0700

    First Commit

---
+++ app/components/ProductCad.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AddToCart from "./AddToCart";
+// import ProductStyle from "./ProductCardStyle.module.css";
+
+const ProductCad = () => {
+  return (
+    <div>
+      <AddToCart />
+    </div>
+  );
+};
+
+export default ProductCad;

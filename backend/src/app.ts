1907115865481233df2305b59c2a231d3a14e32d
marklileo <phornphatmak1112@gmail.com>
2025-03-15 16:51:13 +0700

    feat: add nodemon and file structure

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-// import authRoutes from "./routes/auth.js";
-// import dataRoutes from "./routes/data.js";
+import exampleRoute from "./routes/example_route";
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use("/api/auth", authRoutes);
-// app.use("/api/data", dataRoutes);
-app.get("/", (req, res) => {
-  res.send(`Test using this port ${5001}`);
-});
+app.use("/api/example", exampleRoute);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () =>
--- backend/src/routes/example_route.ts
+++ backend/src/routes/example_route.ts
@@ -1,1 +1,9 @@
-//This is for handle the route path
+import { Router } from "express";
+import { getExample } from "../controllers/example_controller";
+
+const router = Router();
+
+//api/example/get
+router.get("/get", getExample);
+
+export default router;
